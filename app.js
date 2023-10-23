861c1d90879f89760a21cbebc729893c7c8ccb83
jamsheerply <jamsheerpayyoli@gmail.com>
2023-10-23 11:20:09 +0530

    frist

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const app=express()
+
+app.listen(8000,()=>{
+    console.log("server started at port 8000")
+})
+app.get("/",(req,res)=>{
+    res.writeHead("hello")
+})
