097c008e4919364e5c9bab1c0994ec46dcad36db
jamsheerply <jamsheerpayyoli@gmail.com>
2023-10-23 16:23:27 +0530

    ejs setup

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express=require("express")
-const app=express()
+//.................express......................................
+const express = require("express");
+const app = express();
+app.listen(8000, () => {
+    console.log("Server is running at http://127.0.0.1:8000");
+});
 
-app.listen(8000,()=>{
-    console.log("server started at port 8000")
-})
+// ................view engine...................................
+app.set('view engine', 'ejs')
+
+// ....................static file...............................
+app.use(express.static("public"));
+
+
 app.get("/",(req,res)=>{
-    res.writeHead("hello")
+    res.render('user/singleProduct')
+    // res.send("hello")
 })
